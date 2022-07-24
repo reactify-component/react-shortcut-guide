1e58299f4dd7d50ca4ba7bec14d3553dd6e5db18
Innei <tukon479@gmail.com>
2022-07-24 11:14:24 +0800

    fix: filter for safari
    Signed-off-by: Innei <tukon479@gmail.com>

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,4 @@
 module.exports = {
   'postcss-nested': {},
-  'postcss-preset-env': {
-    autoprefixer: {
-      flexbox: 'no-2009',
-    },
-    stage: 3,
-    features: {
-      'custom-properties': false,
-      'nesting-rules': false,
-    },
-  },
+  'postcss-preset-env': {},
 }
